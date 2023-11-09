e6c5d945ff2f457a7ee15515eff999d2e5712ab8
raghurazy <135259028+raghurazy@users.noreply.github.com>
2023-11-09 22:44:24 +0530

    Intro to Middlewares
    Intro to Middlewares

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/add-product");
 });
 
-app.use((req, res, next) => {
-  console.log("second middleware");
-  res.send("<h1>Hello world!</h1>");
+app.use("/add-product", (req, res) => {
+  res.send(
+    `<form action="/product" method="post"><input name="message" type="text"/>
+    <input type="number" name="size"/> <button>submit</button</form>`
+  );
 });
 
 app.listen(3000, () => {
